fae0dc63e571d0a8b5c9fcec9a3a4c39f45c0181
Paul Bruce <me@paulsbruce.io>
2017-06-19 10:55:20 -0400

    First commit

---
+++ app/src/main/java/io/paulsbruce/dev/fingerprintdemo/HomeActivity.java
@@ -0,0 +1,16 @@
+package io.paulsbruce.dev.fingerprintdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
