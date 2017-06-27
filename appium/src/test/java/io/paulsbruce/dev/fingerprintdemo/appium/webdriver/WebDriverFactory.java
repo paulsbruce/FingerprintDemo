f3698a9f6a33cb61aac67cef1cd595b1fa6624e6
Paul Bruce <me@paulsbruce.io>
2017-06-27 16:06:48 -0400

    Add logging to getDriver()

--- appium/src/test/java/io/paulsbruce/dev/fingerprintdemo/appium/webdriver/WebDriverFactory.java
+++ appium/src/test/java/io/paulsbruce/dev/fingerprintdemo/appium/webdriver/WebDriverFactory.java
@@ -2,18 +2,23 @@ package io.paulsbruce.dev.fingerprintdemo.appium.webdriver;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.logging.Logger;
+
 /**
  * Created by paul on 6/22/17.
+ * A factory to provide various web drivers depending on dev/ci/cloud environments; so far, only dev
  */
 
 public class WebDriverFactory {
 
-    public static WebDriver getDriver() throws Exception {
+    public static WebDriver getDriver() {
         WebDriver driver = null;
         try {
             driver = new LocalDevWebDriver();
         } catch(Exception ex) {
-            throw new Exception("APPIUM_ADDRESS: " + System.getenv("APPIUM_ADDRESS"), ex);
+            Logger.getAnonymousLogger().info("APPIUM_ADDRESS: " + System.getenv("APPIUM_ADDRESS"));
+            System.err.println("APPIUM_ADDRESS: " + System.getenv("APPIUM_ADDRESS"));
+            System.err.println(ex.toString());
         }
         assert driver != null;
         return driver;
